e96865366374b34d0399606db2f7e6959092f170
Peter Kiraly <peter.kiral@gmail.com>
2023-02-16 18:00:53 +0100

    final

--- src/app.js
+++ src/app.js
@@ -2,8 +2,23 @@
 
 const ul = document.querySelector("#lista");
 
+const gyumolcsok = [
+    "szilva", 
+    "alma", 
+    "körte", 
+    "barack", 
+    "eper", 
+    "banán"
+];
+
+// 1db bejövő paraméter esetén nem szükséges zárójel a NÉVTELEN függvény bejövő paraméteréhez
+gyumolcsok.forEach( (gyumolcs) => {
+    // console.log(gyumolcs); -- csak teszteléshez kellett...
+    
+    var li = document.createElement("li");
+    li.textContent = gyumolcs;
+    ul.append(li);
+});
+
 // li elemet generálása
 
-var li = document.createElement("li");
-li.textContent = "Szilva";
-ul.append(li);
